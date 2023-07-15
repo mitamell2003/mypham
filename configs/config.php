e8aa1dca1f73e73a661d25badae0e9972cbc4633
gioimtg2003 <conggioi.pro264@gmail.com>
2023-07-15 16:16:52 +0700

    updatte

--- configs/config.php
+++ configs/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST','localhost');
+define('DB_HOST','3.27.144.194');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','conggioi');
 define('DB_NAME','quanlychuoicuahang');
 define('LIMIT_PAGE', 12);
  ?>

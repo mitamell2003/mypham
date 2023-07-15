cb2210c359488257fa9535d89bccc7a55fec35b0
gioimtg2003 <conggioi.pro264@gmail.com>
2023-07-15 16:12:34 +0700

    updatte

--- app/views/404.php
+++ app/views/404.php
@@ -8,9 +8,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Error</title>
 </head>
 <body>
-    <h1>Truy cập sai rồi bro</h1>
+    <h1>Link không tồn tại</h1>
 </body>
 </html>
